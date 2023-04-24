6027a0a68b75079d78d217c1b3aa7bd224cf77c9
Marcos Viana <97001316+Marcosviana91@users.noreply.github.com>
2023-04-24 16:53:24 -0300

    Ajustes visuais
    +Lista descritiva dos cursos em andamento.
    +fundo personalizado de cada instituição certificadora
    +Sessão de certificados expandível e retraível.

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const certificado = document.querySelectorAll('.certificado')
-const ctn_certif_grande = document.getElementById("ctn_certif_grande")
-
-certificado.forEach((div)=>{
-    div.children[1].addEventListener("click", ()=>{
+document.querySelectorAll('.certificado').forEach((div) => {
+    div.children[1].addEventListener("click", () => {
         div.firstElementChild.attributes.src.value
         ctn_certif_grande.firstElementChild.nextElementSibling.setAttribute('src', div.firstElementChild.attributes.src.value)
         ctn_certif_grande.style.display = "flex";
     })
 })
 
-function hide_cert(){
-    ctn_certif_grande.style.display = "none";
+function hide_cert() {
+    document.getElementById("ctn_certif_grande").style.display = "none";
 }
+
+document.querySelectorAll("#certificados h3").forEach(item => {
+    item.addEventListener('click', (evt) => {
+        if (evt.target.nextElementSibling.style.display == 'flex') evt.target.nextElementSibling.style.display = "none"
+        else evt.target.nextElementSibling.style.display = 'flex'
+    })
+})
