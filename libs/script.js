20ae4bfeae93b3595fd84f00c744c8cf96b26a9e
Marcos Viana <97001316+Marcosviana91@users.noreply.github.com>
2023-07-19 13:44:58 -0300

    concluida a sessao de certificados

--- libs/script.js
+++ libs/script.js
@@ -1,18 +1,36 @@
-$(document).ready(function () {
+import {} from "./jquery/jquery-3.7.0.min.js";
+
+$(function () {
     // bootstrap tolltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-    document.querySelectorAll(".tec-used").forEach(function (el){
-        console.log(el);
+    //Certificados
+    $('.certificados h3').each(function (i, el) {
+        $(el.nextElementSibling).hide();
+        $(el).on("click", function (evt) {
+            $(el.nextElementSibling).slideToggle();
+        })
+    })
+
+    //Portifólio
+    $(".tec-used").each(function (indexInArray, el) {
         el.classList.add("badge");
         el.classList.add("rounded-pill");
         el.classList.add("text-bg-primary");
-    })
-    document.querySelectorAll(".tec-not-used").forEach(function (el){
-        console.log(el);
+    });
+    $(".tec-not-used").each(function (indexInArray, el) {
         el.classList.add("badge");
         el.classList.add("rounded-pill");
         el.classList.add("text-bg-secondary");
+    });
+
+    //Modal
+    const modal_certificado = new bootstrap.Modal('#modal_certificado');
+    $('.certificados .card img').each((index, element)=>{
+        $(element).on("click",()=>{
+            $('#modal_certificado img').attr('src', $(element).attr('src').replace('/card', ''));
+            modal_certificado.show();
+        })
     })
 });
