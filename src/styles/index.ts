cf84fc02f6ab8fc27823b386370510a015fceea8
marcosviana91 <marcos.viana.91@gmail.com>
2024-04-17 10:38:47 -0300

    .container: variaveis e breakpoints ok

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components'
+import variaveis from './variaveis'
 
 const EstiloGlobal = createGlobalStyle`
   * {
@@ -7,10 +8,30 @@ const EstiloGlobal = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  *::-webkit-scrollbar {
+    display: none
+  }
+
+  ul li {
+    list-style: none
+  }
+
   .container {
     padding: 0 4px;
-    max-width: 480px;
     margin: 0 auto;
+    max-width: 720px;
+
+    @media (max-width:${variaveis.BRACKPOINTS.DESKTOP}) {
+      max-width: 640px;
+    }
+
+    @media (max-width:${variaveis.BRACKPOINTS.TABLET}) {
+      max-width: 480px;
+    }
+
+    @media (max-width:${variaveis.BRACKPOINTS.MOBILE}) {
+      max-width: 90%;
+    }
   }
 
 
--- src/styles/variaveis.ts
+++ src/styles/variaveis.ts
@@ -1,10 +1,8 @@
 const variaveis = {
-  COR: {
-    VERMELHO: '#C23616',
-    VERDE: '#44BD32',
-    AMARELO: '#F0C11A',
-    LARANJA: '#E1A32A',
-    CINZA: '#2F3640'
+  BRACKPOINTS: {
+    DESKTOP: '1024px',
+    TABLET: '768px',
+    MOBILE: '540px'
   }
 }
 
