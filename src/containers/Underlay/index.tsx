2450649e6f04a2c82acbb2cbd3fbd6548db0dff8
marcosviana91 <marcos.viana.91@gmail.com>
2024-04-17 15:54:05 -0300

    Underlay: Ok

---
+++ src/containers/Underlay/index.tsx
@@ -0,0 +1,122 @@
+import StyledUnderlay from './style'
+import { motion } from 'framer-motion'
+
+function sortDistance() {
+  const distance = ['close', 'far', '']
+  return distance[Math.floor(Math.random() * distance.length)]
+}
+function sortXAnimation() {
+  const X = [
+    ['-10vw', '110vw', '-10vw'],
+    ['110vw', '-10vw', '110vw']
+  ]
+  return X[Math.floor(Math.random() * X.length)]
+}
+
+function sortOpacity() {
+  const opacity = [
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0],
+    [0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1],
+    [0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1],
+    [0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1]
+  ]
+  return opacity[Math.floor(Math.random() * opacity.length)]
+}
+
+function sortDelayAnimation() {
+  return Math.floor(Math.random() * 10)
+}
+
+function sortFontColor() {
+  const color = [
+    '#ffffff',
+    '#ffb35d',
+    '#ffff00',
+    '#ff9100',
+    '#4646ff',
+    '#4cff46',
+    '#ff5dc1',
+    '#ff5d5d',
+    '#5dfff7'
+  ]
+  return color[Math.floor(Math.random() * color.length)]
+}
+
+function sortTechs() {
+  let techs = [
+    'Front-End',
+    'SASS',
+    'LESS',
+    'Gulp',
+    'Grunt',
+    'TypeScript',
+    'React',
+    'Bootstrap',
+    'Tailwind',
+    'Styled-Components',
+    'Framer-Motion',
+    'Back-End',
+    'NGINX',
+    'Docker',
+    'Linux',
+    'Python',
+    'FastAPI',
+    'Uvicorn',
+    'REST',
+    'GaphQL',
+    'SQLModel',
+    'Full-Stack'
+  ]
+  const tempArray = []
+
+  while (techs.length > 0) {
+    const _index = Math.floor(Math.random() * techs.length)
+    tempArray.push(techs[_index])
+    techs = techs.filter((tech) => tech != techs[_index])
+  }
+  console.log(tempArray)
+
+  return tempArray
+}
+
+function Underlay() {
+  return (
+    <StyledUnderlay>
+      <ul>
+        {sortTechs().map((tech, index) => {
+          const distance = sortDistance()
+          let duration = Math.floor(Math.random() * 10) + 30
+          if (distance == 'close') {
+            duration = Math.floor(Math.random() * 10) + 20
+          }
+          if (distance == 'far') {
+            duration = Math.floor(Math.random() * 10) + 40
+          }
+          return (
+            <motion.li
+              key={index}
+              className={distance}
+              initial={{ color: sortFontColor(), opacity: 0 }}
+              animate={{
+                x: sortXAnimation(),
+                opacity: sortOpacity()
+              }}
+              transition={{
+                duration: duration,
+                repeat: Infinity,
+                ease: 'linear',
+                delay: sortDelayAnimation()
+              }}
+            >
+              {tech.toUpperCase()}
+            </motion.li>
+          )
+        })}
+      </ul>
+    </StyledUnderlay>
+  )
+}
+
+export default Underlay
---
+++ src/containers/Underlay/style.tsx
@@ -0,0 +1,34 @@
+import { styled } from 'styled-components'
+
+const StyledUnderlay = styled.div`
+  z-index: -1;
+  background-color: #535353;
+  color: white;
+
+  position: absolute;
+  top: 110px;
+  left: 0;
+  width: 100vw;
+  height: calc(100vh - 143px);
+
+  display: flex;
+  align-items: center;
+
+  ul {
+    li {
+      font-size: 1.5rem;
+      /* text-shadow: 0px 0px 6px white; */
+
+      &.close {
+        font-size: 2rem;
+        /* text-shadow: 4px 4px 8px white; */
+      }
+      &.far {
+        font-size: 1rem;
+        /* text-shadow: 0px 0px 10px white; */
+      }
+    }
+  }
+`
+
+export default StyledUnderlay
