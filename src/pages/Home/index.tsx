6a99a58d8dcadb08c14a1fa1ac218e254fd764eb
marcosviana91 <marcos.viana.91@gmail.com>
2024-04-17 00:04:14 -0300

    preparação co esqueleto

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,59 @@
-const Home = () => <></>
+const Home = () => (
+  <>
+    <section>
+      <h1 id="sobre">Sobre</h1>
+      <p>
+        Apaixonado por tecnologia, sempre a viu como um hobbie, mas somente em
+        2022, aos 31 anos, decidiu fazer a migração de carreira para área de
+        programação.
+      </p>
+      <h2>Atualmente</h2>
+      <ul>
+        <li>
+          <em>Análise e Desenvolvimento de Sistemas</em> - 3º período, na{' '}
+          <a href="https://unifatecie.edu.br/" target="_blank" rel="noreferrer">
+            UniFATECIE
+          </a>
+        </li>
+        <li>
+          <em>Desenvolvedor Full Stack Python</em> - na{' '}
+          <a href="https://ebaconline.com.br/" target="_blank" rel="noreferrer">
+            EBAC
+          </a>
+          .
+        </li>
+      </ul>
+    </section>
+    <section>
+      <h1 id="portifolio">Portifólio</h1>
+      <div>
+        <h2>Front-End</h2>
+      </div>
+      <div>
+        <h2>Back-End</h2>
+      </div>
+    </section>
+    <footer className="w-full flex items-center justify-center h-8">
+      <div>
+        <strong>Marcos Viana</strong> -{' '}
+        <a
+          href="https://www.linkedin.com/in/marcos-viana-full-stack-python/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>{' '}
+        -
+        <a
+          href="https://github.com/Marcosviana91"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </div>
+    </footer>
+  </>
+)
 
 export default Home
